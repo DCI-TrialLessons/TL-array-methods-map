daf5044ad4347f8560b0f51837036f1f7a7ded73
Benjamin-DCI <47890043+Benjamin-DCI@users.noreply.github.com>
2022-02-09 15:43:06 +0100

    Initialize autograding-solution

---
+++ __tests__/tasks.1.js
@@ -0,0 +1,28 @@
+const rewire = require("rewire");
+const fs = require("fs");
+const content = fs
+    .readFileSync("./index.js")
+    .toString("utf-8")
+    .replace(/ ()/g, "");
+const solution = rewire("../index.js");
+
+beforeAll(() => (consoleSpy = jest.spyOn(console, "log")));
+
+describe('1. Camelize', () => {
+    it("Should accept a string and return Camelized version", () => {
+        const camelize = solution.__get__("camelize");
+        expect(camelize("background-color")).toBe("backgroundColor");
+    })
+}) 
+
+describe('2. User Object', () => {
+    it("Should return a new user object with 'id, fullName'", () => {
+        const usersMapped = solution.__get__("usersMapped");
+        expect(usersMapped[0].id).toBe(1);
+        expect(usersMapped[0].fullName).toBe("John Smith");
+        expect(usersMapped[1].id).toBe(2);
+        expect(usersMapped[1].fullName).toBe("Pete Hunt");
+        expect(usersMapped[2].id).toBe(3);
+        expect(usersMapped[2].fullName).toBe("Mary Key");
+    })
+})
